54d0efeba9d4feb23bf056327232f53ab395b35e
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-07-21 02:10:10 +0200

    create map with api, added leaflet lib

---
+++ odour-inputs/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import Map from './map';
+
+function App() {
+  return (
+  <Map/>
+  );
+}
+
+export default App;
